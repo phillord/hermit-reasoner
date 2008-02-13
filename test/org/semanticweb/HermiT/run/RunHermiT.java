c372208316c59ff6e320a58bda9c0f08cc123068
bmotik <bmotik@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2008-02-13 12:13:55 +0000

    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@86 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- test/org/semanticweb/HermiT/run/RunHermiT.java
+++ test/org/semanticweb/HermiT/run/RunHermiT.java
@@ -20,32 +20,24 @@ public class RunHermiT {
         
         HermiT hermit=new HermiT();
 //        hermit.setBlockingSignatureCacheType(HermiT.BlockingSignatureCacheType.NOT_CACHED);
-//        hermit.setExistentialsType(HermiT.ExistentialsType.INDIVIDUAL_REUSE);
+        hermit.setExistentialsType(HermiT.ExistentialsType.INDIVIDUAL_REUSE);
         hermit.setTimingOn();
 //        hermit.setTimingWithPauseOn();
 //        hermit.setDebuggingOn(false);
 //        hermit.setDebuggingOn(true);
 
-        hermit.loadOntology("file:/C:/Work/TestOntologies/DOLCE_397/dolce_all.owl");
-//        hermit.loadOntology("file:/C:/Work/ontologies/NCI/nciOncology.owl");
-//        hermit.loadOntology("file:/C:/Temp/full-galen-no-functionality.owl");
-//        hermit.loadOntology("file:/C:/Temp/galen-module1-no-functionality.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/FMA/fma_no_data.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/pizza/pizza.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/propreo/propreo.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/NCI/nciOncology.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/DOLCE_397/dolce_all_no_datatype.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/bams-from-swanson/bams-from-swanson-98-4-5-07-no-data.owl");
+//        hermit.loadOntology("file:/C:/Work/TestOntologies/GALEN/galen-module1-no-functionality.owl");
+        hermit.loadOntology("file:/C:/Work/TestOntologies/GALEN/galen-module1-no-inverse.owl");
 //        hermit.loadOntology("file:/C:/Work/TestOntologies/GALEN/galen-module1.owl");
 //        hermit.loadOntology("file:/C:/Work/TestOntologies/GALEN/galen-ians-full-undoctored.owl");
 //        hermit.loadOntology("file:/C:/Work/TestOntologies/GALEN/galen-ians-full-doctored.owl");
 //        hermit.loadOntology("file:/C:/Work/TestOntologies/wine/wine-no-data-properties.owl");
-
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\GALEN-minus-graph.ser"));
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\FMA-minus-graph.ser"));
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\graphs\\galen-ians-full-undoctored-modified-concrete.ser"));
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\FMA-Full-concrete.ser"));
-
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\galen-module1.ser"));
-//        hermit.getDLOntology().save(new java.io.File("C:\\Temp\\galen-module1.ser"));
-//        hermit.loadDLOntology(new java.io.File("C:\\Temp\\galen-ians-full-undoctored.ser"));
-//        hermit.getDLOntology().save(new java.io.File("C:\\Temp\\galen-ians-full-undoctored.ser"));
-//        hermit=HermiT.load(new java.io.File("C:\\Temp\\galen-ians-full-undoctored-good-run.ser"));
-//        hermit=HermiT.load(new java.io.File("C:\\Temp\\galen-ians-full-undoctored-bad-run.ser"));
 
         PrintWriter printWriter=new PrintWriter("c:\\temp\\rules.txt");
         printWriter.println(hermit.getDLOntology().toString(hermit.getNamespaces()));
@@ -57,6 +49,20 @@ public class RunHermiT {
         }
         long start=System.currentTimeMillis();
 
+//        hermit.isABoxSatisfiable();
+          subsumptionHierarchy=hermit.getSubsumptionHierarchy();
+        
+//        hermit.isSatisfiable("http://nlm.nih.gov/ontology/FMAInOWL#Diaphragmatic_surface_of_heart");
+//        hermit.isSatisfiable("http://nlm.nih.gov/ontology/FMAInOWL#A1_pulley_of_fibrous_tendon_sheath_of_finger");
+//        hermit.isSubsumedBy("http://nlm.nih.gov/ontology/FMAInOWL#A1_pulley_of_fibrous_tendon_sheath_of_finger","http://nlm.nih.gov/ontology/FMAInOWL#A-tubule_of_triplet_microtubule_of_kinetosome_of_flagellu");
+        
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-agent","http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl#physical-endurant");
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-agent","http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#agentive-social-object");
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-physical-object","http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-agent");      
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-agent","http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl#non-physical-endurant");
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#rational-agent","http://www.loa-cnr.it/ontologies/ExtendedDnS.owl#agentive-social-object");
+//        hermit.isSubsumedBy("http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl#non-physical-endurant","http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl#endurant");
+        
         /* These are the hard tests in 'galen-ians-full-undoctored.owl'. They require backtracking in the presence of individual reuse. */
 //        hermit.isSubsumedBy("http://www.co-ode.org/ontologies/galen#AbdominalCavity","http://www.co-ode.org/ontologies/galen#ActualCavity");
 //        hermit.isSubsumedBy("http://www.co-ode.org/ontologies/galen#AbdominalAorta","http://www.co-ode.org/ontologies/galen#ArteryWhichHasLaterality");
@@ -65,6 +71,8 @@ public class RunHermiT {
 //        hermit.isSubsumedBy("http://www.co-ode.org/ontologies/galen#RenalAbscess","http://www.co-ode.org/ontologies/galen#ActualCavity");
 //        hermit.isSubsumedBy("http://www.co-ode.org/ontologies/galen#RightBundleBranchBlock","http://www.co-ode.org/ontologies/galen#LeftBundleBranchBlock");
 
+        
+//        hermit.getTableau().isSatisfiable(org.semanticweb.HermiT.model.AtomicConcept.create("http://nlm.nih.gov/ontology/FMAInOWL#A-peripheral_microtubule_of_axoneme_of_cilium"));
 //        hermit.getTableau().isSatisfiable(org.semanticweb.HermiT.model.AtomicConcept.create("http://www.co-ode.org/ontologies/galen#Abdomen"));
 //        hermit.isSatisfiable("http://www.co-ode.org/ontologies/galen#Abdomen");
 //        hermit.isSatisfiable("http://www.co-ode.org/ontologies/galen#AbdominalCavity");
@@ -73,8 +81,6 @@ public class RunHermiT {
 //        hermit.isSatisfiable("http://www.co-ode.org/ontologies/galen#AcuteErosionOfStomach");
 //        hermit.isSatisfiable("http://www.co-ode.org/ontologies/galen#AnteriorCruciateLigament");
       
-        subsumptionHierarchy=hermit.getSubsumptionHierarchy();
-
         long duration=System.currentTimeMillis()-start;
         System.out.println("The reasoning task took "+duration+" ms");
 
