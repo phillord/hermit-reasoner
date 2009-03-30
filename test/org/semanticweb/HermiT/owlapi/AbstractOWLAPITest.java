ff159a9749ab85031c848002516e59b4b043d670
bmotik <bmotik@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2009-03-30 13:49:11 +0000

    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@409 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- test/org/semanticweb/HermiT/owlapi/AbstractOWLAPITest.java
+++ test/org/semanticweb/HermiT/owlapi/AbstractOWLAPITest.java
@@ -1,56 +1,21 @@
 package org.semanticweb.HermiT.owlapi;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Set;
 
 import junit.framework.AssertionFailedError;
 
-
 import org.semanticweb.HermiT.AbstractOWLOntologyTest;
-import org.semanticweb.HermiT.Configuration;
-import org.semanticweb.HermiT.blocking.AnywhereBlocking;
-import org.semanticweb.HermiT.blocking.BlockingSignatureCache;
-import org.semanticweb.HermiT.blocking.BlockingStrategy;
-import org.semanticweb.HermiT.blocking.DirectBlockingChecker;
-import org.semanticweb.HermiT.blocking.PairWiseDirectBlockingChecker;
-import org.semanticweb.HermiT.existentials.CreationOrderStrategy;
-import org.semanticweb.HermiT.existentials.ExpansionStrategy;
-import org.semanticweb.HermiT.model.DLOntology;
-import org.semanticweb.HermiT.model.DescriptionGraph;
-import org.semanticweb.HermiT.structural.OWLClausification;
-import org.semanticweb.HermiT.tableau.Node;
-import org.semanticweb.HermiT.tableau.Tableau;
 
 public abstract class AbstractOWLAPITest extends AbstractOWLOntologyTest {
-    protected static final Node[][] NO_TUPLES=new Node[0][];
-    protected static final String[] IGNORED_CLAUSES= { " :- owl:BottomDataProperty*(X,Y)"," :- owl:BottomObjectProperty(X,Y)" };
+    protected static final String[] IGNORED_CLAUSES={
+        " :- owl:BottomDataProperty*(X,Y)",
+        " :- owl:BottomObjectProperty(X,Y)"
+    };
 
     public AbstractOWLAPITest(String name) {
         super(name);
     }
-
-    /**
-     * @return a clausified version of the loaded ontology
-     * @throws Exception
-     */
-    protected DLOntology getDLOntology() throws Exception {
-        OWLClausification clausifier=new OWLClausification(new Configuration());
-        Set<DescriptionGraph> noDescriptionGraphs=Collections.emptySet();
-        return clausifier.clausify(m_ontologyManager,m_ontology,noDescriptionGraphs);
-    }
-
-    protected Tableau getTableau() throws Exception {
-        DLOntology dlOntology=getDLOntology();
-        DirectBlockingChecker directBlockingChecker=PairWiseDirectBlockingChecker.INSTANCE;
-        BlockingSignatureCache blockingSignatureCache=new BlockingSignatureCache(directBlockingChecker);
-        BlockingStrategy blockingStrategy=new AnywhereBlocking(directBlockingChecker,blockingSignatureCache);
-        ExpansionStrategy ExpansionStrategy=new CreationOrderStrategy(blockingStrategy);
-        return new Tableau(null,ExpansionStrategy,dlOntology,new HashMap<String,Object>());
-    }
-
     /**
      * tests that the sets have equal length and that the actual set contains all objects from the control set, otherwise the test fails and the contents of the control and the actual set are printed
      */
