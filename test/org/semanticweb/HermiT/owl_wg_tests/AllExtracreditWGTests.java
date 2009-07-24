ba177b9b22994757c06ad522a9de4be458e57ff3
bglimm <bglimm@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2009-07-24 11:57:08 +0000

    added class to run all extracredit tests
    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@595 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

---
+++ test/org/semanticweb/HermiT/owl_wg_tests/AllExtracreditWGTests.java
@@ -0,0 +1,16 @@
+package org.semanticweb.HermiT.owl_wg_tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllExtracreditWGTests {
+    public static Test suite() throws Exception {
+        WGTestRegistry wgTestRegistry=new WGTestRegistry();
+        TestSuite suite=new TestSuite("OWL WG Non-Rejected Tests");
+        for (WGTestDescriptor wgTestDescriptor : wgTestRegistry.getTestDescriptors())
+            if (wgTestDescriptor.status==WGTestDescriptor.Status.EXTRACREDIT) {
+                    wgTestDescriptor.addTestsToSuite(suite);
+            }
+        return suite;
+    }
+}
