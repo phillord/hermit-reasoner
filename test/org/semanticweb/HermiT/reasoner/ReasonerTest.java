d83c0a33d2a39f9cf3331cb478872b8490121482
bglimm <bglimm@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2010-01-14 17:50:54 +0000

    HermiT has a bug with sameIndividual axioms
    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@710 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- test/org/semanticweb/HermiT/reasoner/ReasonerTest.java
+++ test/org/semanticweb/HermiT/reasoner/ReasonerTest.java
@@ -29,24 +29,38 @@ public class ReasonerTest extends AbstractReasonerTest {
     public ReasonerTest(String name) {
         super(name);
     }
-    public void testLearningBackTracking() throws Exception {
-        String axioms = "SubClassOf(owl:Thing ObjectIntersectionOf(ObjectUnionOf(:C :D1) ObjectUnionOf(:C :D2) ObjectUnionOf(:C :D3) ObjectUnionOf(:C :D4) ObjectUnionOf(:C :D5) ObjectSomeValuesFrom(:r ObjectAllValuesFrom(ObjectInverseOf(:r) ObjectComplementOf(:C)))))"
-            + "ClassAssertion(:a1 :A)"
-            + "ClassAssertion(:a2 :A)"
-            + "ClassAssertion(:a3 :A)"
-            + "ClassAssertion(:a4 :A)"
-            + "ClassAssertion(:a5 :A)"
-            + "ClassAssertion(:a6 :B)"
-            + "ClassAssertion(:a7 :B)"
-            + "ClassAssertion(:a8 :B)"
-            + "ClassAssertion(:a9 :B)"
-            + "ClassAssertion(:a10 :A)"
-            + "SubClassOf(:A  ObjectMinCardinality(20 :s :B))"
-            + "SubClassOf(:B  ObjectMinCardinality(20 :s :A))";
+    public void testSameAs() throws Exception {
+        String axioms = "Declaration(NamedIndividual(:a1))"
+        	+ "Declaration(NamedIndividual(:a2))"
+        	+ "Declaration(Class(:A))"
+        	+ "ClassAssertion(:A :a1)"
+            + "SameIndividual(:a1 :a2)";
         loadOntologyWithAxioms(axioms);
         createReasoner();
-        assertTrue(m_reasoner.isConsistent());
+        OWLNamedIndividual a2=m_dataFactory.getOWLNamedIndividual(IRI.create(AbstractReasonerTest.NS + "a2"));
+        OWLClass A = m_dataFactory.getOWLClass(IRI.create(AbstractReasonerTest.NS + "A"));
+        assertTrue(m_reasoner.hasType(a2, A, false));
     }
+//    public void testLearningBackTracking() throws Exception {
+//        String axioms = "SubClassOf(owl:Thing ObjectIntersectionOf(ObjectUnionOf(:C :D1) ObjectUnionOf(:C :D2) ObjectUnionOf(:C :D3) ObjectUnionOf(:C :D4) ObjectUnionOf(:C :D5) ObjectSomeValuesFrom(:r ObjectAllValuesFrom(ObjectInverseOf(:r) ObjectComplementOf(:C)))))"
+//            + "ClassAssertion(:A :a1)"
+//            + "ClassAssertion(:A :a2)"
+//            + "ClassAssertion(:A :a3)"
+//            + "ClassAssertion(:A :a4)"
+//            + "ClassAssertion(:A :a5)"
+//            + "ClassAssertion(:B :a6)"
+//            + "ClassAssertion(:B :a7)"
+//            + "ClassAssertion(:B :a8)"
+//            + "ClassAssertion(:B :a9)"
+//            + "ClassAssertion(:A :a10)"
+//            + "SubClassOf(:A  ObjectMinCardinality(20 :s :B))"
+//            + "SubClassOf(:B  ObjectMinCardinality(20 :s :A))";
+//        loadOntologyWithAxioms(axioms);
+//        Configuration c=new Configuration();
+//        c.useDisjunctionLearning=false;
+//        createReasoner(c,null);
+//        assertTrue(m_reasoner.isConsistent());
+//    }
     public void testObjectPropertDomains() throws Exception {
         String axioms = "SubClassOf(:A :B)"
             + "ObjectPropertyDomain(:r :A)";
