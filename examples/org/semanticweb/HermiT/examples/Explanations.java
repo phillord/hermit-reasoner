8d135f6232a9cc07d1949737d1e844a449bb1c38
bglimm <bglimm@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2010-03-30 09:33:20 +0000

    resolved conflict on the explanation example and updated it to work as soon as the OWL API explanation is fixed
    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@787 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- examples/org/semanticweb/HermiT/examples/Explanations.java
+++ examples/org/semanticweb/HermiT/examples/Explanations.java
@@ -9,9 +9,7 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
@@ -20,48 +18,49 @@ import com.clarkparsia.owlapi.explanation.BlackBoxExplanation;
 import com.clarkparsia.owlapi.explanation.HSTExplanationGenerator;
 
 public class Explanations {
+    // *****************************************
+    // Note that the example is not working at the moment due to a bug in the OWL API. 
+    // *****************************************
     public static void main(String[] args) throws Exception {
-        // First, we create an OWLOntologyManager object. The manager will load and
-        // save ontologies.
+        // First, we create an OWLOntologyManager object. The manager will load and 
+        // save ontologies. 
         OWLOntologyManager manager=OWLManager.createOWLOntologyManager();
         // We will create several things, so we save an instance of the data factory
         OWLDataFactory dataFactory=manager.getOWLDataFactory();
-        // Now, we create the file from which the ontology will be loaded.
+        // Now, we create the file from which the ontology will be loaded. 
         // Here the ontology is stored in a file locally in the ontologies subfolder
         // of the examples folder.
         File inputOntologyFile = new File("examples/ontologies/pizza.owl");
-        // We use the OWL API to load the ontology.
+        // We use the OWL API to load the ontology. 
         OWLOntology ontology=manager.loadOntologyFromOntologyDocument(inputOntologyFile);
-
-        // Lets make things worth and turn Pizza into an inconsistent ontology by asserting that the
-        // unsatisfiable icecream class has some instance.
-        // First, create an instance of the OWLClass object for the icecream class.
+        
+        // Lets make things worth and turn Pizza into an inconsistent ontology by asserting that the 
+        // unsatisfiable icecream class has some instance. 
+        // First, create an instance of the OWLClass object for the unsatisfiable  icecream class.
         IRI icecreamIRI=IRI.create("http://www.co-ode.org/ontologies/pizza/pizza.owl#IceCream");
         OWLClass icecream=dataFactory.getOWLClass(icecreamIRI);
-        // Create some instance of the icecream class
-        IRI strawberryIceCreamIRI=IRI.create("http://www.co-ode.org/ontologies/pizza/pizza.owl#strawberryIceCream");
-        OWLNamedIndividual strawberryIceCream=dataFactory.getOWLNamedIndividual(strawberryIceCreamIRI);
-        // Create a class assertion axiom
-        OWLClassAssertionAxiom ax=dataFactory.getOWLClassAssertionAxiom(icecream, strawberryIceCream);
-        manager.addAxiom(ontology, ax);
-
-        // Now we can start and create the reasoner. Since materialisation of axioms is controlled
-        // by OWL API classes and is not natively supported by HermiT, we need to instantiate HermiT
-        // as an OWLReasoner. This is done via a ReasonerFactory object.
+        // Now we can start and create the reasoner. Since explanation is not natively supported by 
+        // HermiT and is realised in the OWL API, we need to instantiate HermiT 
+        // as an OWLReasoner. This is done via a ReasonerFactory object. 
         ReasonerFactory factory = new ReasonerFactory();
-        // We don't want HermiT to thrown an exception for inconsistent ontologies because then we
-        // can't explain the inconsistency. This can be controlled via a configuration setting.
+        // We don't want HermiT to thrown an exception for inconsistent ontologies because then we 
+        // can't explain the inconsistency. This can be controlled via a configuration setting.  
         Configuration configuration=new Configuration();
         configuration.throwInconsistentOntologyException=false;
-        // The factory can now be used to obtain an instance of HermiT as an OWLReasoner.
+        // The factory can now be used to obtain an instance of HermiT as an OWLReasoner. 
         OWLReasoner reasoner=factory.createReasoner(ontology, configuration);
-
+        // Let us confirm that icecream is indeed unsatisfiable:
+        System.out.println("Is icecream satisfiable? "+reasoner.isSatisfiable(icecream)); 
+        // Now we instantiate the explanation classes
         BlackBoxExplanation exp=new BlackBoxExplanation(ontology, factory, reasoner);
         HSTExplanationGenerator multExplanator=new HSTExplanationGenerator(exp);
-        multExplanator.getExplanations(icecream);
+        // Now we can get explanations for the unsatisfiability. 
         Set<Set<OWLAxiom>> explanations=multExplanator.getExplanations(icecream);
+        // Let us print them. Each explanation is one possible set of axioms that cause the 
+        // unsatisfiability. 
         for (Set<OWLAxiom> explanation : explanations) {
             System.out.println("------------------");
+            System.out.println("Axioms causing the unsatisfiability: ");
             for (OWLAxiom causingAxiom : explanation) {
                 System.out.println(causingAxiom);
             }
