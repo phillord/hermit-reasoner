50beb7b5c3b59f6a5fce5be38a2dea515a7b604c
bglimm <bglimm@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2012-11-08 10:57:50 +0000

    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@1077 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- test/org/semanticweb/HermiT/tableau/AllTests.java
+++ test/org/semanticweb/HermiT/tableau/AllTests.java
@@ -14,6 +14,7 @@ public class AllTests {
         suite.addTestSuite(DependencySetTest.class);
         suite.addTestSuite(NIRuleTest.class);
         suite.addTestSuite(MergeTest.class);
+        suite.addTestSuite(GraphTest.class);
         //$JUnit-END$
         return suite;
     }
