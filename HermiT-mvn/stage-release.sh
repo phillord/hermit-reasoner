68cb32bdcf8103949429995a5d2bc2859817b461
plord <plord@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2013-11-06 08:53:12 +0000

    From branch
    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@1128 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

---
+++ HermiT-mvn/stage-release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VN=1.3.8.3
+
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -DrepositoryId=sonatype-nexus-staging -DuseAgent=true \
+    -DpomFile=pom.xml -Dfile=target/org.semanticweb.hermit-$VN.jar
+
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -DrepositoryId=sonatype-nexus-staging -Dclassifier=javadoc \
+    -DuseAgent=true \
+    -DpomFile=pom.xml -Dfile=target/org.semanticweb.hermit-$VN-javadoc.jar
+
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -DrepositoryId=sonatype-nexus-staging -Dclassifier=sources \
+    -DuseAgent=true \
+    -DpomFile=pom.xml -Dfile=target/org.semanticweb.hermit-$VN-sources.jar
