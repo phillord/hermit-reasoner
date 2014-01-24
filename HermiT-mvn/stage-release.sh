78b32e5e80989cf62e8231bdad523cd99d127abe
plord <plord@6b6e81c3-f1c2-4030-a0a9-a0569132be5c>
2014-01-24 14:34:35 +0000

    1.3.8.4 release
    git-svn-id: svn://edison.comlab.ox.ac.uk/krr/2008/HermiT/trunk@1136 6b6e81c3-f1c2-4030-a0a9-a0569132be5c

--- HermiT-mvn/stage-release.sh
+++ HermiT-mvn/stage-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VN=1.3.8.3
+VN=1.3.8.4
 
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
     -DrepositoryId=sonatype-nexus-staging -DuseAgent=true \
