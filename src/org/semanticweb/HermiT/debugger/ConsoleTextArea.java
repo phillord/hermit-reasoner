4fdab667bb123d9536bb7a470fa75eb2edae94d1
ignazio <ignazio.palmisano@gmail.com>
2015-11-21 23:41:45 +0000

    Bug: looking up elements by value rather than key
    FindBugs spotted incompatible types used to look up data in a map

--- src/org/semanticweb/HermiT/debugger/ConsoleTextArea.java
+++ src/org/semanticweb/HermiT/debugger/ConsoleTextArea.java
@@ -191,7 +191,6 @@ public class ConsoleTextArea extends JTextArea {
                 }
                 string.getChars(0,string.length(),m_buffer,m_firstFreeChar);
                 m_firstFreeChar+=string.length();
-                notifyAll();
             }
         }
         @Override
